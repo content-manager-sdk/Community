c53520960dbb3b20fa231a70acdd47bb345e64c4
dchurchland <dchurchland@gmail.com>
2017-02-21 16:56:55 +1100

    Added CORS Module

---
+++ Samples/ServiceAPI/CSharp/CORSModule/CORSModule.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+
+using System.Web;
+// THANK YOU: 
+// * https://evolpin.wordpress.com/2012/10/12/the-cors/
+// * Thomas Tran
+
+public class CORSModule : IHttpModule
+{
+    private string[] origins;
+    private string methods = "POST,GET,OPTIONS";
+    private string headers = "Content-Type, Authorization";
+
+    public void Init(HttpApplication context)
+    {
+
+        //Get list of origin domains
+        var originConfigs = ConfigurationManager.AppSettings["allowedOrigins"];
+
+
+        this.origins = (originConfigs ?? "").Split(',').Select(o => o.Trim()).ToArray();
+
+        context.PreSendRequestHeaders += delegate
+        {
+
+            var response = context.Response;
+
+           #region try to build the current url requested by user
+           var referredUrl = "";
+
+            if (context.Request.UrlReferrer != null)
+            {
+                referredUrl = context.Request.UrlReferrer.Scheme + "://" + context.Request.UrlReferrer.Host;
+                if (context.Request.UrlReferrer.Port != 80)
+                {
+                    referredUrl = referredUrl + ":" + context.Request.UrlReferrer.Port;
+                }
+            }
+           #endregion
+
+           //check the current origin within the list from config file. 
+           //if yes, add it to the response header
+           if (this.origins.Contains(referredUrl, StringComparer.InvariantCultureIgnoreCase))
+            {
+                response.AppendHeader("Access-Control-Allow-Origin", referredUrl);
+                response.AppendHeader("Access-Control-Allow-Credentials", "true");
+
+                if (!string.IsNullOrWhiteSpace(this.methods))
+                {
+                    response.AppendHeader("Access-Control-Allow-Methods", this.methods);
+                }
+                if (!string.IsNullOrWhiteSpace(this.headers))
+                {
+                    response.AppendHeader("Access-Control-Allow-Headers", this.headers);
+                }
+            }
+           //otherwise allow all origins by default if no origins set in appSettings
+           else if (this.origins.Length == 0)
+            {
+                response.AppendHeader("Access-Control-Allow-Origin", "*");
+                response.AppendHeader("Access-Control-Allow-Credentials", "false");
+
+                if (!string.IsNullOrWhiteSpace(this.methods))
+                {
+                    response.AppendHeader("Access-Control-Allow-Methods", this.methods);
+                }
+
+                if (!string.IsNullOrWhiteSpace(this.headers))
+                {
+                    response.AppendHeader("Access-Control-Allow-Headers", this.headers);
+                }
+            }
+
+
+           //fix preflight
+           if (context.Request.HttpMethod == "OPTIONS")
+            {
+                response.StatusCode = (int)HttpStatusCode.OK;
+            }
+
+        };
+
+
+    }
+
+    public void Dispose()
+    {
+
+    }
+}
+
---
+++ Samples/ServiceAPI/CSharp/CORSModule/ReadMe.md
@@ -0,0 +1,49 @@
+# CORS ServiceAPI Plugin
+Cross origin resource sharing (CORS) allows a javascript application in one domain to talk to a web service in anotehyr domain, something which the browser security model usually prevents.  The ServiceAPI documentation mentions a CORS feature which, in theory, will enable CORS support but in most circumstances will not work.  This module adds CORS support by getting into the request pipeline before the standard IIS authentication has had a chance to block the CORS pre-flight request.
+
+## Setup
+To use this sample:
+ - build the project
+ - copy CORSModule.dll to the ServiceAPI bin folder
+ - add the module to the web.config, as seen below
+ - if you are using windows authentication you may need to explicitly set the supported domains, in fact it is probably a good idea to do this any, set these in appSettings as seen below
+ - if you do not set 'allowedOrigins' in app settings than all domains will be allowed to use CORS (Access-Control-Allow-Origin == "*")
+
+## Module in web.config
+ ```xml
+ <modules runAllManagedModulesForAllRequests="true">
+  <add name="CORSModule" type="CORSModule, CORSModule" />
+  ...
+</modules>
+ ```
+
+## appSettings
+```xml
+  <appSettings file="user.config">
+    <add key="allowedOrigins" value="https://MyDomainOne, https://MyDomainTwo"/>
+  </appSettings>
+```
+
+## Sample jQuery
+The following jQuery will make a windows authenticated request on your server, note nothing special needs to be sent from the jQuery to support CORS.
+
+```javascript
+$.ajax({
+    xhrFields :{ withCredentials:true },
+    url: "http://MyDomainOne/ServiceAPI82/Record/1?&properties=RecordTitle",
+    type: 'GET',
+    dataType: 'json',
+})
+.done(function (response, statusText) {
+    
+    if (statusText === "success") {
+        alert("Record named: " + response.Results[0].RecordTitle.Value + " was found.");
+    }
+})
+.fail(function (xhr) {
+    var err = eval("(" + xhr.responseText + ")");
+    alert(err.ResponseStatus.Message);
+
+});
+
+```
