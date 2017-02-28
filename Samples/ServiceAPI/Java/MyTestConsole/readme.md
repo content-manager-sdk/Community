4df2d1bd8818d8aae4d01f44eca93836fee204c7
dchurchland <dchurchland@gmail.com>
2017-02-28 13:51:40 +1100

    Java Client - example of trustedToImpersonate

--- Samples/ServiceAPI/Java/MyTestConsole/readme.md
+++ Samples/ServiceAPI/Java/MyTestConsole/readme.md
@@ -12,3 +12,32 @@ The metadata URL that the plugin calls will look similar to the sample below.  I
 ```
 http://YOUR_SERVER/ServiceAPI/types/java?Package=com.acme&GlobalNamespace=dto
 ```
+
+# Impersonation
+Java applications are often server sie applications that handle their own authentication, in which case you might need to simply pass the authenticated user name to the ServiceAPI and expect it to trust you.  To allow this follow these steps:
+
+## Set hptrim.config in ServiceAPI
+Set the trustedToImpersonate property to the name that the Java application will authenticate as.
+```
+<hptrim
+  serviceFeatures="Razor,Html,Json,Xml,PredefinedRoutes"
+  trustedToImpersonate="domain\\ImpersonatingUser"
+  ...>
+```  
+## Authenticate as the trusted user
+In your application authenticate as the trusted user set in hotrim.config
+```
+		client = new JsonServiceClient("http://192.168.0.19/ServiceAPI");
+		client.setCredentials("domain\\ImpersonatingUser", "YOUR_PASSWORD");
+```  
+
+## Pass user name in HTTP header
+Set a request filter on the client so that all request are sent with the userToImpersonate header containing the login name of the user you wish to run as.
+```
+		client.RequestFilter = new ConnectionFilter() {
+            @Override
+            public void exec(HttpURLConnection conn) {
+                conn.addRequestProperty("userToImpersonate", "itu_tenduser");
+            }
+        };
+```
--- Samples/ServiceAPI/Java/MyTestConsole/src/com/acme/MyTestConsole.java
+++ Samples/ServiceAPI/Java/MyTestConsole/src/com/acme/MyTestConsole.java
@@ -1,5 +1,6 @@
 package com.acme;
 
+import java.net.HttpURLConnection;
 import java.util.ArrayList;
 import java.util.HashMap;
 
@@ -7,9 +8,13 @@ import com.acme.dto.Records;
 import com.acme.dto.RecordsResponse;
 import com.acme.dto.TrimStringProperty;
 import com.acme.dto.FieldDefinition;
+import com.acme.dto.Location;
+import com.acme.dto.LocationFind;
+import com.acme.dto.LocationsResponse;
 import com.acme.dto.Record;
 import com.acme.dto.RecordTypeRef;
 
+import net.servicestack.client.ConnectionFilter;
 import net.servicestack.client.JsonServiceClient;
 import net.servicestack.client.WebServiceException;
 
@@ -17,14 +22,45 @@ public class MyTestConsole {
 
 	private static JsonServiceClient client;
 
-	private static ArrayList<String> makePropertyList() {
+	private static ArrayList<String> makePropertyList(String... propertyNames) {
 		ArrayList<String> properties = new ArrayList<String>();
-		properties.add("RecordTitle");
-		properties.add("RecordNumber");
-
+		
+		if (propertyNames.length > 0) {
+			for (String propertyName : propertyNames) {
+				properties.add(propertyName);
+			}
+		} else {
+			properties.add("RecordTitle");
+			properties.add("RecordNumber");
+		}
 		return properties;
 	}
 
+	@SuppressWarnings("unused")
+	private static void getMyName() {
+		// Fetch my Location object
+		
+
+		LocationFind request = new LocationFind();
+		request.Id = "me";
+		
+
+		request.Properties = makePropertyList("SortName");
+
+		try {
+			LocationsResponse response = client.get(request);
+
+			for (Location loc : response.Results) {
+				System.out.println(loc.SortName.Value);
+			}
+
+		} catch (WebServiceException ex) {
+			System.out.println(ex.getErrorMessage());
+		}
+		
+
+	}
+	
 	@SuppressWarnings("unused")
 	private static void recordSearch() {
 		// Fetch a record
@@ -53,7 +89,7 @@ public class MyTestConsole {
 		record.setRecordType(recordType);
 
 		TrimStringProperty titleProp = new TrimStringProperty();
-		titleProp.setValue("test title");
+		titleProp.setValue("test title 68");
 		record.setTitle(titleProp);
 
 		// this tells the request that we want to return the title and number of the 
@@ -103,15 +139,28 @@ public class MyTestConsole {
 	public static void main(String[] arguments) {
 		System.out.println("Hello, world");
 
-		client = new JsonServiceClient("http://192.168.0.19/ServiceAPI91");
+		client = new JsonServiceClient("http://192.168.0.19/ServiceAPI");
 		client.setCredentials("YOUR_USERNAME", "YOUR_PASSWORD");
+		
+		// if the user specified in setCredentials has been given permission to impersonate via the hptrim.config
+		// option 'trustedToImpersonate' then you may use the code below to impersonate any user.
+/*		
+		client.RequestFilter = new ConnectionFilter() {
+            @Override
+            public void exec(HttpURLConnection conn) {
+                conn.addRequestProperty("userToImpersonate", "itu_tenduser");
+            }
+        };
+*/		
 
 		// this next line is important for basic authentication
 		client.setAlwaysSendBasicAuthHeaders(true);
 
+		getMyName();
+		
 		// recordSearch();
-		// recordCreate();
-		recordCreateWithAdditionalField();
+		 //recordCreate();
+		//recordCreateWithAdditionalField();
 
 	}
 }
