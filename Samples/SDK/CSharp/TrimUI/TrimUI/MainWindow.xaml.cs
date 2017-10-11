3d9d5df8b26ea8866b1b05a60fcd3e4f2c9a9765
dchurchland <dchurchland@gmail.com>
2017-10-11 13:38:07 +1100

    Trim UI Sample

---
+++ Samples/SDK/CSharp/TrimUI/TrimUI/MainWindow.xaml.cs
@@ -0,0 +1,118 @@
+﻿using HP.HPTRIM.SDK;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace TestWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        /// <summary>
+        /// Retrieves a handle to the desktop window. TRIM UI components require the handle of a parent window
+        /// </summary>
+        /// <returns></returns>
+        [System.Runtime.InteropServices.DllImport("user32.dll", EntryPoint = "GetDesktopWindow")]
+        public static extern IntPtr GetDesktopWindow();
+
+        Database _database = null;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            try
+            {
+                TrimApplication.Initialize();
+
+                _database = new Database();             
+                _database.Connect();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        public override void BeginInit()
+        {
+
+            base.BeginInit();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Record record = new Record(_database, 9000000000);
+
+            if (HP.HPTRIM.SDK.PropertyEditor.EditModal(GetDesktopWindow(), record))
+            {
+                record.Save();
+            }
+
+        }
+
+        protected override void OnClosing(System.ComponentModel.CancelEventArgs e)
+        {
+            if (_database != null)
+            {
+                _database.Dispose();
+                _database = null;
+            }
+            base.OnClosing(e);
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            TrimMainObjectSearch search = new TrimMainObjectSearch(_database, BaseObjectTypes.Record);
+            search.SelectAll();
+
+            ObjectSelector.DisplayResults(GetDesktopWindow(), search);
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            TrimMainObjectSearch search = new TrimMainObjectSearch(_database, BaseObjectTypes.Record);
+
+
+            TrimSearchClause recentClause = new TrimSearchClause(_database, BaseObjectTypes.Record, SearchClauseIds.RecordMyDocuments);
+            search.AddSearchClause(recentClause);
+
+            search.SetPurpose((int)RecordPurposes.FindTemplate, (int)AllEnumerations.WordOpenFileTypes);
+
+
+
+
+            var selectedSearch = ObjectSelector.SelectOne(GetDesktopWindow(), search);
+
+            if (selectedSearch != null)
+            {
+                string gg = selectedSearch.Uri.ToString();
+            }
+        }
+
+        private void Button_Click_3(object sender, RoutedEventArgs e)
+        {
+            DesktopHelper helper = new DesktopHelper();
+            Database newDatabase = helper.SelectDatabase(GetDesktopWindow(), false, false);
+
+            if (newDatabase != null)
+            {
+                _database.Dispose();
+                _database = newDatabase;
+            }
+        }
+    }
+}
---
+++ Samples/SDK/CSharp/TrimUI/TrimUI/ReadMe.md
@@ -0,0 +1,17 @@
+﻿# User Interface components
+Components to find and edit a TrimMainObject (e.g. Record) are available in the SDK.
+
+## Take Care
+Each of these methods requires a pointer to a parent window.  It is important to send something meaningful not simply null, which can cause the SDK some confusion.
+
+
+## Running
+To run:
+ - download the code
+ - copy HP.HPTRIM.SDK.dll to the lib folder
+ - if you are running a 32 bit OS select 'Prefer 32-bit' in the project properties
+ - run the project
+
+You should see a screen like the one below.
+
+![UI](TrimUI.PNG)
