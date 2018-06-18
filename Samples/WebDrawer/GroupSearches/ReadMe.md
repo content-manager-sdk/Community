94baa804e9e47333c2e8ae90f1d0963a319a1a51
dchurchland <dchurchland@gmail.com>
2018-06-18 10:08:02 +1000

    Add WebDrawer search grouping sample

---
+++ Samples/WebDrawer/GroupSearches/ReadMe.md
@@ -0,0 +1,30 @@
+# Group Search Clauses in WebDrawer
+
+## Overview
+The ebDrawer search form allows you to specify three search clauses and seperate them with either AND or OR boolean operators.  What it does not allow for is explicit grouping, so you will not be sure whether the first two clauses will be evaluated together or th last two..
+
+This sample allows parentheses to be added to the search string and also displays the search string so you can know exactly what search will happen before pressing search.
+
+
+
+## Installing the sample
+
+To install from the files here:
+  * copy grouped_search.js to your WebDrawer 'scripts' folder
+  * in Views\Search.cshtml add `<script src="~/scripts/grouped_search.js" type="text/javascript"></script>` to the scripts section (see image 1 below)
+
+
+ ![image 1](script.PNG)
+ 
+## Add custom elements to search page
+In the file Views\Shared\SearchForm.cshtml add the following HTML before the 'Sort By' heading
+```
+    <label class="checkbox inline"><input type="checkbox" id="exact-string-searches" checked /> @Translations.lang.search_search_exact</label>
+
+    <div>
+        <label class="radio inline"><input type="radio" name="clause-grouping" id="group-first-two" checked /> Group first two </label>
+        <label class="radio inline"> <input type="radio" name="clause-grouping" id="group-last-two" /> Group last two</label>
+    </div>
+
+    <div style="margin-top:10px">Search query: <em id="sampleQuery"></em></div>
+```
---
+++ Samples/WebDrawer/GroupSearches/scripts/grouped_search.js
@@ -0,0 +1,160 @@
+﻿
+var makeQuery = function () {
+    var value = []; // $("#default-search-query").val();
+    var exactMatch = $('#exact-string-searches');
+    exactMatch = exactMatch && exactMatch[0] && exactMatch[0].checked;
+
+    var gropuFirstTwo = $('group-first-two').prop('checked');
+
+    var lastBool = 'AND';
+    try {
+        var lastBool;
+        $('.clause-selector').each(function () {
+            var fld = $(this).parents('.control-group').first().find('.search-clause').first();
+            //===================keep track of the last selected field's format so that we can use later while setting lastBool
+            var last_field_format = null;
+            $(this).children('option').filter("option:selected").each(function () {
+
+                var searchClause = $(this).val();
+
+                if (searchClause) {
+                    var fldFormat = $(this).parameterFormat();
+                    var fldValue = fld.val();
+                    if (fldFormat && (fldValue || fldFormat === 'Boolean')) {
+                        if (
+                            fldValue.indexOf(':') < 0
+                            && fldFormat !== 'BigNumber'
+                            && fldFormat !== 'Number'
+                            && fldFormat !== 'Date'
+                            && fldFormat !== 'Datetime'
+                        ) {
+
+                            if (exactMatch) {
+                                fldValue = '"' + fldValue + '"';
+                            }
+
+                            var objectType = $(this).objectTypeParameter();
+
+                            var wordSearchClause;
+
+                            if (objectType === 'Classification') {
+                                wordSearchClause = 'plnWord';
+                            }
+
+                            if (objectType === 'Schedule') {
+                                wordSearchClause = 'schWord';
+                            }
+
+                            if (objectType === 'Record') {
+                                wordSearchClause = 'recTitle';
+                            }
+
+                            if (wordSearchClause) {
+                                fldValue = '[' + wordSearchClause + ':' + fldValue + '] OR ' + fldValue;
+                            }
+
+                        }
+                        last_field_format = fldFormat;
+                        if (fldFormat !== 'Boolean') {
+                            searchClause += ":" + fldValue + '';
+                        }
+                        if (value.length > 0) {
+                            value.push(lastBool);
+                        }
+                        value.push(searchClause);
+
+                    }
+                }
+
+            });
+            if (last_field_format && (last_field_format === 'Date' || last_field_format === 'Datetime'))
+                lastBool = fld.next().next().children('.active').attr('name');
+            else
+                lastBool = fld.next().children('.active').attr('name');
+        });
+
+        if (value.length > 0) {
+            if (value.length >= 5) {
+                if ($('#group-first-two').attr('checked')) {
+                    value.splice(3, 0, ")");
+                    value.splice(0, 0, "(");
+                } else {
+                    value.push(')');
+                    value.splice(2, 0, "(");
+                }
+            }
+            var defaultValue = $("#default-search-query").val();
+            if (defaultValue) {
+                return defaultValue + " AND (" + value.join(" ") + ")";
+            } else {
+                return value.join(" ");
+            }
+        }
+        return;
+    }
+    catch (error) {
+        console.log(error);
+        return;
+    }
+}
+
+$(function () {
+
+
+
+
+    var searchString = "";
+    $("#search-form").off('submit').on("submit", function (event) {
+
+        var value = makeQuery();
+
+        var sortBy = [];
+
+        if (value) {
+            try {
+
+                $('.sortby-selector').each(function () {
+                    var ascending = $(this).next('label').first().children(':checked').length > 0 ? "-" : "";
+                    $(this).children('option').filter("option:selected").each(function () {
+                        var sortClause = $(this).val();
+
+                        if (sortClause) {
+                            sortBy.push(sortClause + ascending);
+                        }
+                    });
+                })
+
+            }
+            catch (error) {
+                console.log(error);
+                return false;
+            }
+        }
+
+        if (!value) {
+            // if value has not been populated do nothing;
+            return false;
+        }
+
+        $("#search-query").val(value);
+        if (sortBy.length > 0) {
+            $('#sortBy').val(sortBy.join(','));
+        }
+
+
+    });
+
+
+    var onFormChange = function () {
+        var searchQuery = makeQuery();
+        var displayQuery = $('#sampleQuery');
+
+        if ($('#sampleQuery').text() !== searchQuery) {
+            $('#sampleQuery').text(searchQuery);
+        }
+    }
+
+    setInterval(onFormChange, 1000);
+
+
+});
