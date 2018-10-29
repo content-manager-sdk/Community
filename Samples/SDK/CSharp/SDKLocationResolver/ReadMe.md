e5f4255da09e8d312dcf2fe6b93a3598f22cb51b
dchurchland <dchurchland@gmail.com>
2018-10-29 13:21:13 +1100

    add SDK Location resolver

---
+++ Samples/SDK/CSharp/SDKLocationResolver/ReadMe.md
@@ -0,0 +1,21 @@
+﻿# Import using Origin
+This is a somewhat redundant application as the TRIM client includes the Document Queue feature which is a more comprehensive document import tool.  Origins do have some capabilities not included in the Document Queue so may be of interest.  In addition this demonstrates how easy it is to build a simple windows application using the SDK.
+
+## Operation
+This sample allows the user to select an Origin and a Windows folder and then have any files in that folder imported to HPE CM and then deleted from the folder on a schedule.  As Origins may only be created by users with elevated priveldges this sample allows the user to select a pre-defined Origin and then choose which folder it will import from.
+
+## Folder generation
+One thing that Origins allow for that Document Queues do not is the auto creation of new containers based on a set of rules.  This sample shows this in action if you configure the Origin appropriately.
+## SDK Usage
+The parts of the SDK used in this sample include UI components, Origins and TrimMainObjectSearch.
+
+## Running
+To run:
+ - download the code
+ - copy HP.HPTRIM.SDK.dll to the lib folder
+ - if you are running a 32 bit OS select 'Prefer 32-bit' in the project properties
+ - run the project
+
+You should see a screen like the one below.
+
+![Importer UI](record_importer.PNG)
---
+++ Samples/SDK/CSharp/SDKLocationResolver/SDKLocationResolver/Program.cs
@@ -0,0 +1,85 @@
+﻿using HP.HPTRIM.SDK;
+using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SDKLocationResolver
+{
+    class Program
+    {
+        private static string FindInstalledTRIMPath()
+        {
+            RegistryKey[] registryHives =
+            {
+            Registry.LocalMachine,
+            Registry.CurrentUser
+        };
+
+            string[] registryPaths =
+            {
+            @"SOFTWARE\Micro Focus\Content Manager\MSISettings",
+            @"SOFTWARE\Hewlett-Packard\HP TRIM\MSISettings"
+        };
+
+            string INSTALLDIR = "INSTALLDIR";
+
+            foreach (var hive in registryHives)
+            {
+                foreach (var regPath in registryPaths)
+                {
+                    RegistryKey msiSettingsKey = hive.OpenSubKey(regPath);
+                    if (msiSettingsKey != null)
+                    {
+                        string installDir = msiSettingsKey.GetValue(INSTALLDIR) as string;
+                        if (installDir != null && installDir.Length > 0)
+                        {
+                            return installDir;
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+
+        private static Assembly AssemblyResolveEventHandler(object sender, ResolveEventArgs args, string pathToSdkDll)
+        {
+            if (args.Name.Contains("HP.HPTRIM.SDK"))
+            {
+                Assembly assembly = Assembly.LoadFrom(pathToSdkDll);
+                return assembly;
+            }
+            return null;
+        }
+
+
+        static void doSomething()
+        {
+            TrimApplication.Initialize();
+
+            Database db = new Database();
+            db.WorkgroupServerName = "local";
+            db.Id = "L1";
+            db.Connect();
+
+            Console.WriteLine(db.CurrentUser.SortName);
+        }
+
+        static void Main(string[] args)
+        {
+            string installDir = FindInstalledTRIMPath();
+            string pathToSdkDll = Path.Combine(installDir, "HP.HPTRIM.SDK.dll");
+            AppDomain.CurrentDomain.AssemblyResolve += (sender, e) => AssemblyResolveEventHandler(sender, e, pathToSdkDll);
+
+
+
+            doSomething();
+
+        }
+    }
+}
