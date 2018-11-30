9065e9e547912b7c23f400f514ad9b6cd107bbca
dchurchland <dchurchland@gmail.com>
2018-11-30 12:17:09 +1100

    add HttpClient sample

---
+++ Samples/ServiceAPI/CSharp/RecordCreationWithHttpClient/ReadMe.md
@@ -0,0 +1,2 @@
+# Create Record using System.Net.HttpClient
+A very simple example using the Http classes you would expect to find in a Xamarin application.
---
+++ Samples/ServiceAPI/CSharp/RecordCreationWithHttpClient/RecordCreationWithHttpClient/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecordCreationWithHttpClient
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+
+			using (var handler = new HttpClientHandler { UseDefaultCredentials = true })
+			{
+				HttpClient httpClient = new HttpClient(handler);
+				httpClient.DefaultRequestHeaders.Add("Accept", "application/json"); //tell the ServiceAPI we want a JSON response
+
+				httpClient.BaseAddress = new Uri("http://localhost/ServiceAPI/");
+
+				// send some JSON and tell the request it is JSON via the mediaType parameter
+				var content = new System.Net.Http.StringContent("{\"RecordRecordType\":{\"Uri\":2}, \"RecordTypedTitle\":\"Test Record\"}", Encoding.UTF8, "application/json");
+
+
+				HttpResponseMessage response = null;
+				string responseJson = null;
+
+				// run some async code inside a non-async method
+				Task.Run(async () =>
+				{
+					response = await httpClient.PostAsync("Record", content);
+					responseJson = await response.Content.ReadAsStringAsync();
+
+
+				}).GetAwaiter().GetResult();
+
+				Console.WriteLine(responseJson);
+			}
+		}
+	}
+}
