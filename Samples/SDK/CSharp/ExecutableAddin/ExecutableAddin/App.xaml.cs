1f4f8658792122a2a294d28d17f73b482ca0774a
dchurchland <dchurchland@gmail.com>
2019-07-31 11:41:04 +1000

    update addin ReadMe.md

--- Samples/SDK/CSharp/ExecutableAddin/ExecutableAddin/App.xaml.cs
+++ Samples/SDK/CSharp/ExecutableAddin/ExecutableAddin/App.xaml.cs
@@ -9,35 +9,66 @@ using System.Windows;
 
 namespace ExexutableAddin
 {
-    /// <summary>
-    /// Interaction logic for App.xaml
-    /// </summary>
-    public partial class App : Application
-    {
-        private void Application_Startup(object sender, StartupEventArgs e)
-        {
+	/// <summary>
+	/// Interaction logic for App.xaml
+	/// </summary>
+	public partial class App : Application
+	{
+		private void Application_Startup(object sender, StartupEventArgs e)
+		{
 
-            try
-            {
-                TrimApplication.Initialize();
+			try
+			{
+				TrimApplication.Initialize();
 
-                if (e.Args.Length < 4)
-                {
-                    MessageBox.Show("Please tag one or more records.");
-                }
-                else
-                {
 
-                    MainWindow wnd = new MainWindow(e.Args[1], e.Args[3]);
-                    wnd.Show();
-                }
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show(ex.Message);
-            }
-        }
+				string uris = null;
+				string dbid = null;
 
-        
-    }
+				for (int counter = 0; counter < e.Args.Length; counter++)
+				{
+					if (e.Args[counter] == "--dbid")
+					{
+						dbid = e.Args[counter + 1];
+					}
+
+					if (e.Args[counter] == "--uris")
+					{
+						uris = e.Args[counter + 1];
+					}
+				}
+
+				if (string.IsNullOrEmpty(uris))
+				{
+					MessageBox.Show("Please tag one or more records.", "Addin", MessageBoxButton.OK, MessageBoxImage.Information);
+					return;
+				}
+
+				if (string.IsNullOrEmpty(dbid))
+				{
+					MessageBox.Show("Addin incorrectly configured, no database ID sent.", "Error", MessageBoxButton.OK, MessageBoxImage.Error);
+					return;
+				}
+
+
+				// if uris is a comma separated list do nothing, otherwise read the file containing the list of URIs
+				long tmp;
+				if (!Int64.TryParse(uris.Split(',').First(), out tmp))
+				{
+					// the temp file contains a Newline separated list of URIs, so I replace newlines with commas which is what MainWindow expects.
+					uris = System.IO.File.ReadAllText(uris).Replace(System.Environment.NewLine, ",");
+				}
+
+				MainWindow wnd = new MainWindow(dbid, uris);
+				wnd.Show();
+
+			}
+			catch (Exception ex)
+			{
+				MessageBox.Show(ex.Message);
+			}
+		}
+
+
+	}
 }
--- Samples/SDK/CSharp/ExecutableAddin/ReadMe.md
+++ Samples/SDK/CSharp/ExecutableAddin/ReadMe.md
@@ -2,7 +2,7 @@
 The executuable addin uses command line arguments to get the database ID and selected URIs.  If you include the database ID (see below) the paramters will be in this format: ```--dbid J1 --uris 9000000001,9000000002```
 
 ## Warning
-As the URIS are passed as a comma separated string there is a limit on the number that will be passed.  I believe it is 200, make sure you test this.
+As the URIS are passed as a comma separated string there is a limit on the number that will be passed.   If you require the executable add-in to support large numbers of selected records then CM 9.4 has an option to 'send tagged URIs as a temporary file.  This sample uses command line arguments to get the database ID and selected URIs.  If you include the database ID (see below) the paramters will be in this format: ```--dbid J1 --uris c:\Users\USER\AppData\Local\Micro Focus\Content Manager\TEMP\HPTRIM.340064\t0001CR5.txt```
 
 ## Rationale
 One reason to use the executable over a .Net DLL is that you can store the executable once on a shared drive and have all users access the same instance remotely.  In general the .Net addin provides better functionality.
@@ -14,3 +14,6 @@ This is a very simple sample.  The points of interest are:
  
 ## Example configuration screen
 ![configuration](exeaddin.PNG)
+
+## Example configuration screen using temp file
+![configuration](exeaddin_large_sel.PNG)
