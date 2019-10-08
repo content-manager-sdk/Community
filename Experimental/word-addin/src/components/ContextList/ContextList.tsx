f20113bbfae55f47425b3aaab1eeccf96631a638
dchurchland <dchurchland@gmail.com>
2019-10-08 16:33:34 +1100

    Word Addin - add search bar component

--- Experimental/word-addin/src/components/ContextList/ContextList.tsx
+++ Experimental/word-addin/src/components/ContextList/ContextList.tsx
@@ -7,7 +7,6 @@ import {
 	ITrimConnector,
 	ITrimMainObject,
 	IEnumDetails,
-	ISearchClauseOrFieldDef,
 } from "src/trim-coms/trim-connector";
 import { IContextualMenuProps } from "office-ui-fabric-react/lib/ContextualMenu";
 
@@ -16,19 +15,9 @@ import { createRef } from "office-ui-fabric-react/lib/Utilities";
 import { BaseObjectTypes } from "../../trim-coms/trim-baseobjecttypes";
 import TrimObjectSearchList from "../TrimObjectSearchList/TrimObjectSearchList";
 import { ITrimObjectSearchList } from "../TrimObjectSearchList/TrimObjectSearchList.types";
-import {
-	ComboBox,
-	IComboBox,
-	IComboBoxOption,
-	mergeStyles,
-	FocusTrapZone,
-	SelectableOptionMenuItemType,
-	TextField,
-} from "office-ui-fabric-react";
+import { mergeStyles, FocusTrapZone } from "office-ui-fabric-react";
 
-import { debounce } from "throttle-debounce";
-
-const config = (global as any).config;
+import SearchBar from "../SearchBar/SearchBar";
 
 const wrapperClassName = mergeStyles({
 	selectors: {
@@ -65,13 +54,9 @@ export class ContextList extends React.Component<
 	{
 		selectRecord: ITrimMainObject;
 		searchQuery: string;
-		searchType: string;
-		searchTypeOptions: IComboBoxOption[];
 	}
 > {
 	private _searchList = createRef<ITrimObjectSearchList>();
-	private _basicComboBox = React.createRef<IComboBox>();
-	autocompleteSearchDebounced: any;
 
 	constructor(props: {
 		appStore?: any;
@@ -82,102 +67,8 @@ export class ContextList extends React.Component<
 
 		this.state = {
 			selectRecord: { Uri: 0 },
-			searchQuery: this._getQuery("container"),
-			searchType: "goto",
-			searchTypeOptions: [],
+			searchQuery: "",
 		};
-
-		this.autocompleteSearchDebounced = debounce(1000, this.__textChanged);
-	}
-
-	componentDidMount() {
-		const groupBy = function(list: any, keyGetter: any) {
-			const map = new Map();
-			list.forEach((item: any) => {
-				const key = keyGetter(item);
-				const collection = map.get(key);
-				if (!collection) {
-					map.set(key, [item]);
-				} else {
-					collection.push(item);
-				}
-			});
-			return map;
-		};
-
-		const { trimConnector } = this.props;
-		const searchClauses = config.SEARCH_CLAUSES || ["anyWord", "content"];
-		let searchTypeOptions: IComboBoxOption[] = [{ key: "goto", text: "Show" }];
-		const trimType = BaseObjectTypes.Record;
-		let lastGroup = "";
-		trimConnector!.getSearchClauseOrFieldDefinitions(trimType).then((data) => {
-			groupBy(data, function(sc: any) {
-				return sc.MethodGroup;
-			}).forEach((clauseDefs: ISearchClauseOrFieldDef[]) => {
-				clauseDefs.forEach((clauseDef: ISearchClauseOrFieldDef) => {
-					if (
-						clauseDef.IsFavorite ||
-						clauseDef.IsRecent ||
-						searchClauses.includes(clauseDef.ClauseName)
-					) {
-						if (lastGroup !== clauseDef.MethodGroup) {
-							lastGroup = clauseDef.MethodGroup;
-							searchTypeOptions.push({
-								key: clauseDef.MethodGroup,
-								text: clauseDef.MethodGroup,
-								itemType: SelectableOptionMenuItemType.Header,
-							});
-						}
-						searchTypeOptions.push({
-							key: clauseDef.ClauseName,
-							text: clauseDef.Caption,
-							data: clauseDef,
-						});
-					}
-				});
-			});
-
-			this.setState({ searchTypeOptions: searchTypeOptions });
-		});
-	}
-
-	private getComboOptions = (): IComboBoxOption[] => {
-		const { appStore } = this.props;
-
-		return [
-			// {
-			// 	key: "Header1",
-			// 	text: "Navigate to",
-			// 	itemType: SelectableOptionMenuItemType.Header,
-			// },
-			{ key: "container", text: appStore.messages.web_in_Same_Container },
-			{ key: "contacts", text: appStore.messages.web_with_Same_Contacts },
-			{ key: "related", text: appStore.messages.web_Related_To },
-			{ key: "all_related", text: appStore.messages.web_All_Related_To },
-			// {
-			// 	key: "Header2",
-			// 	text: "Quick search",
-			// 	itemType: SelectableOptionMenuItemType.Header,
-			// },
-			// { key: "search_content", text: appStore.messages.web_Search_Content },
-		];
-	};
-
-	private _getQuery(key: string) {
-		if (key.startsWith("search_")) {
-			return "";
-		}
-
-		const { appStore } = this.props;
-
-		const searches = {
-			container: `recContainerEx:[recContainsEx:${appStore!.RecordUri}]`,
-			contacts: `recSameContact:${appStore.RecordUri}`,
-			related: `recRelated:${appStore.RecordUri}`,
-			all_related: `recAllRelated:${appStore.RecordUri}`,
-		};
-
-		return searches[key];
 	}
 
 	private _trimObjectSelected = (trimObject: ITrimMainObject) => {
@@ -209,96 +100,9 @@ export class ContextList extends React.Component<
 		}
 	};
 
-	private _comboChange = (
-		event: React.FormEvent<IComboBox>,
-		option?: IComboBoxOption,
-		index?: number,
-		value?: string
-	): void => {
-		if (option) {
-			this.setState({ searchQuery: this._getQuery(option!.key as string) });
-		}
-	};
-
-	private _comboChangeSearchType = (
-		event: React.FormEvent<IComboBox>,
-		option?: IComboBoxOption,
-		index?: number,
-		value?: string
-	): void => {
-		if (option) {
-			const key = option.key as string;
-
-			if (key === "goto") {
-				this.setState({
-					searchQuery: this._getQuery("container"),
-					searchType: key,
-				});
-			} else {
-				if (
-					option.data &&
-					(option.data.ParameterFormat === "Boolean" ||
-						option.data.SearchParameterFormat === "Boolean")
-				) {
-					this.setState({ searchQuery: option.text, searchType: key });
-				} else {
-					this.setState({ searchQuery: "", searchType: key });
-
-					if (this._basicComboBox.current) {
-						this._basicComboBox.current.focus();
-					}
-				}
-			}
-		}
-	};
-
-	private __textChanged = (value: string): void => {
-		this.setState({ searchQuery: value! });
-	};
-
-	private _pendingChange = (
-		option?: IComboBoxOption,
-		index?: number,
-		value?: string
-	): void => {
-		if (value) {
-			this.autocompleteSearchDebounced(value);
-		}
-	};
-
-	private _textChange = (newValue: any): void => {
-		if (newValue) {
-			this.autocompleteSearchDebounced(newValue);
-		}
-	};
-
-	private _findCannedSearch = (searchQuery: string): number => {
-		const comboOptions = this.getComboOptions();
-		for (let counter = 0; counter < comboOptions.length; counter++) {
-			if (searchQuery === this._getQuery(comboOptions[counter].key as string)) {
-				return counter;
-			}
-		}
-		return -1;
-	};
-
 	public render() {
 		const { appStore, trimConnector } = this.props;
-		const {
-			searchQuery,
-			searchType,
-			selectRecord,
-			searchTypeOptions,
-		} = this.state;
-
-		const comboOptions = this.getComboOptions();
-		const cannedSearchNumber = this._findCannedSearch(searchQuery);
-		const defaultProps =
-			cannedSearchNumber > -1
-				? {
-						selectedKey: comboOptions[cannedSearchNumber].key,
-				  }
-				: { value: searchQuery };
+		const { searchQuery, selectRecord } = this.state;
 
 		const contextMenuProps: IContextualMenuProps = {
 			items: [
@@ -353,26 +157,6 @@ export class ContextList extends React.Component<
 			],
 		};
 
-		let fullSearchQuery = searchQuery;
-		let searchFormat = "";
-
-		if (searchType != "goto") {
-			searchTypeOptions.forEach((so) => {
-				if (so.key === searchType) {
-					searchFormat =
-						so.data.SearchParameterFormat || so.data.ParameterFormat;
-					if (
-						so.data.ParameterFormat === "Boolean" ||
-						so.data.SearchParameterFormat === "Boolean"
-					) {
-						fullSearchQuery = so.text;
-					} else if (fullSearchQuery) {
-						fullSearchQuery = so.text + ":" + fullSearchQuery;
-					}
-				}
-			});
-		}
-
 		return (
 			<div className={wrapperClassName}>
 				<IconButton
@@ -382,30 +166,13 @@ export class ContextList extends React.Component<
 					split={false}
 				/>
 				<h3>
-					<ComboBox
-						className="context-list-title"
-						options={searchTypeOptions}
-						selectedKey={searchType}
-						onChange={this._comboChangeSearchType}
+					<SearchBar
+						trimType={BaseObjectTypes.Record}
+						onChange={(newValue) => {
+							this.setState({ searchQuery: newValue });
+						}}
+						includeShortCuts={true}
 					/>
-
-					{searchType === "goto" ? (
-						<ComboBox
-							{...defaultProps}
-							options={comboOptions}
-							componentRef={this._basicComboBox}
-							onChange={this._comboChange}
-							allowFreeform={false}
-							autoComplete="off"
-							onPendingValueChanged={this._pendingChange}
-							className="trim-search-query"
-						/>
-					) : searchFormat === "Boolean" ? null : (
-						<TextField
-							onBeforeChange={this._textChange}
-							className="trim-search-text"
-						/>
-					)}
 				</h3>
 
 				<FocusTrapZone
@@ -415,7 +182,7 @@ export class ContextList extends React.Component<
 					<TrimObjectSearchList
 						componentRef={this._searchList}
 						trimType={BaseObjectTypes.Record}
-						q={fullSearchQuery}
+						q={searchQuery}
 						contentsInReverseDateOrder={true}
 						includeAlternateWhenShowingFolderContents={true}
 						excludeShortCuts={true}
---
+++ Experimental/word-addin/src/components/SearchBar/SearchBar.tsx
@@ -0,0 +1,303 @@
+import * as React from "react";
+import { inject, observer } from "mobx-react";
+import "./SearchBar.css";
+
+import {
+	ITrimConnector,
+	ISearchClauseOrFieldDef,
+} from "src/trim-coms/trim-connector";
+
+import { BaseObjectTypes } from "../../trim-coms/trim-baseobjecttypes";
+
+import {
+	ComboBox,
+	IComboBox,
+	IComboBoxOption,
+	SelectableOptionMenuItemType,
+	TextField,
+} from "office-ui-fabric-react";
+
+import { debounce } from "throttle-debounce";
+
+const config = (global as any).config;
+
+export class SearchBar extends React.Component<
+	{
+		appStore?: any;
+		trimConnector?: ITrimConnector;
+		className?: string;
+		trimType: BaseObjectTypes;
+		onChange?: (newQuery: string) => void;
+		includeShortCuts: boolean;
+	},
+	{
+		searchQuery: string;
+		searchType: string;
+		searchTypeOptions: IComboBoxOption[];
+		searchFormat: string;
+	}
+> {
+	private _basicComboBox = React.createRef<IComboBox>();
+	autocompleteSearchDebounced: any;
+
+	constructor(props: {
+		appStore?: any;
+		trimConnector?: ITrimConnector;
+		className?: string;
+		trimType: BaseObjectTypes;
+		includeShortCuts: boolean;
+	}) {
+		super(props);
+
+		this.state = {
+			searchQuery: this._getQuery("container"),
+			searchType: "goto",
+			searchTypeOptions: [],
+			searchFormat: "",
+		};
+
+		this.autocompleteSearchDebounced = debounce(1000, this.__textChanged);
+	}
+
+	componentDidMount() {
+		const { trimType, includeShortCuts, onChange } = this.props;
+
+		if (includeShortCuts && onChange) {
+			onChange(this._getQuery("container"));
+		}
+
+		const groupBy = function(list: any, keyGetter: any) {
+			const map = new Map();
+			list.forEach((item: any) => {
+				const key = keyGetter(item);
+				const collection = map.get(key);
+				if (!collection) {
+					map.set(key, [item]);
+				} else {
+					collection.push(item);
+				}
+			});
+			return map;
+		};
+
+		const { trimConnector } = this.props;
+		const searchClauses = config.SEARCH_CLAUSES || ["anyWord", "content"];
+		let searchTypeOptions: IComboBoxOption[] = includeShortCuts
+			? [{ key: "goto", text: "Show" }]
+			: [];
+
+		let lastGroup = "";
+		trimConnector!.getSearchClauseOrFieldDefinitions(trimType).then((data) => {
+			groupBy(data, function(sc: any) {
+				return sc.MethodGroup;
+			}).forEach((clauseDefs: ISearchClauseOrFieldDef[]) => {
+				clauseDefs.forEach((clauseDef: ISearchClauseOrFieldDef) => {
+					if (
+						clauseDef.IsFavorite ||
+						clauseDef.IsRecent ||
+						searchClauses.includes(clauseDef.ClauseName)
+					) {
+						if (lastGroup !== clauseDef.MethodGroup) {
+							lastGroup = clauseDef.MethodGroup;
+							searchTypeOptions.push({
+								key: clauseDef.MethodGroup,
+								text: clauseDef.MethodGroup,
+								itemType: SelectableOptionMenuItemType.Header,
+							});
+						}
+						searchTypeOptions.push({
+							key: clauseDef.ClauseName,
+							text: clauseDef.Caption,
+							data: clauseDef,
+						});
+					}
+				});
+			});
+
+			this.setState({ searchTypeOptions: searchTypeOptions });
+		});
+	}
+
+	private getComboOptions = (): IComboBoxOption[] => {
+		const { appStore } = this.props;
+
+		return [
+			{ key: "container", text: appStore.messages.web_in_Same_Container },
+			{ key: "contacts", text: appStore.messages.web_with_Same_Contacts },
+			{ key: "related", text: appStore.messages.web_Related_To },
+			{ key: "all_related", text: appStore.messages.web_All_Related_To },
+		];
+	};
+
+	private _getQuery(key: string) {
+		if (key.startsWith("search_")) {
+			return "";
+		}
+
+		const { appStore } = this.props;
+
+		const searches = {
+			container: `recContainerEx:[recContainsEx:${appStore!.RecordUri}]`,
+			contacts: `recSameContact:${appStore.RecordUri}`,
+			related: `recRelated:${appStore.RecordUri}`,
+			all_related: `recAllRelated:${appStore.RecordUri}`,
+		};
+
+		return searches[key];
+	}
+
+	private _comboChange = (
+		event: React.FormEvent<IComboBox>,
+		option?: IComboBoxOption,
+		index?: number,
+		value?: string
+	): void => {
+		if (option) {
+			this.callChange(this._getQuery(option!.key as string));
+		}
+	};
+
+	private _comboChangeSearchType = (
+		event: React.FormEvent<IComboBox>,
+		option?: IComboBoxOption,
+		index?: number,
+		value?: string
+	): void => {
+		if (option) {
+			const key = option.key as string;
+
+			if (key === "goto") {
+				this.callChange(this._getQuery("container"), key);
+			} else {
+				if (
+					option.data &&
+					(option.data.ParameterFormat === "Boolean" ||
+						option.data.SearchParameterFormat === "Boolean")
+				) {
+					this.callChange(option.text, key);
+				} else {
+					this.callChange("", key);
+
+					if (this._basicComboBox.current) {
+						this._basicComboBox.current.focus();
+					}
+				}
+			}
+		}
+	};
+
+	private callChange(newQuery: string, newSearchType?: string) {
+		const { onChange } = this.props;
+		const { searchTypeOptions, searchType } = this.state;
+
+		const thisSearchType = newSearchType || searchType;
+
+		if (onChange) {
+			let fullSearchQuery = newQuery;
+
+			if (thisSearchType != "goto") {
+				searchTypeOptions.forEach((so) => {
+					if (so.key === thisSearchType) {
+						this.setState({
+							searchFormat:
+								so.data.SearchParameterFormat || so.data.ParameterFormat,
+						});
+						if (
+							so.data.ParameterFormat === "Boolean" ||
+							so.data.SearchParameterFormat === "Boolean"
+						) {
+							onChange(so.text);
+						} else if (fullSearchQuery) {
+							onChange(so.key + ":" + fullSearchQuery);
+						}
+					}
+				});
+			}
+		}
+
+		if (newSearchType) {
+			this.setState({ searchQuery: newQuery, searchType: newSearchType });
+		} else {
+			this.setState({ searchQuery: newQuery });
+		}
+	}
+
+	private __textChanged = (value: string): void => {
+		this.callChange(value!);
+	};
+
+	private _pendingChange = (
+		option?: IComboBoxOption,
+		index?: number,
+		value?: string
+	): void => {
+		if (value) {
+			this.autocompleteSearchDebounced(value);
+		}
+	};
+
+	private _textChange = (newValue: any): void => {
+		if (newValue) {
+			this.autocompleteSearchDebounced(newValue);
+		}
+	};
+
+	private _findCannedSearch = (searchQuery: string): number => {
+		const comboOptions = this.getComboOptions();
+		for (let counter = 0; counter < comboOptions.length; counter++) {
+			if (searchQuery === this._getQuery(comboOptions[counter].key as string)) {
+				return counter;
+			}
+		}
+		return -1;
+	};
+
+	public render() {
+		const {
+			searchQuery,
+			searchType,
+			searchTypeOptions,
+			searchFormat,
+		} = this.state;
+
+		const comboOptions = this.getComboOptions();
+		const cannedSearchNumber = this._findCannedSearch(searchQuery);
+		const defaultProps =
+			cannedSearchNumber > -1
+				? {
+						selectedKey: comboOptions[cannedSearchNumber].key,
+				  }
+				: { value: searchQuery };
+
+		return (
+			<span>
+				<ComboBox
+					className="context-list-title"
+					options={searchTypeOptions}
+					selectedKey={searchType}
+					onChange={this._comboChangeSearchType}
+				/>
+
+				{searchType === "goto" ? (
+					<ComboBox
+						{...defaultProps}
+						options={comboOptions}
+						componentRef={this._basicComboBox}
+						onChange={this._comboChange}
+						allowFreeform={false}
+						autoComplete="off"
+						onPendingValueChanged={this._pendingChange}
+						className="trim-search-query"
+					/>
+				) : searchFormat === "Boolean" ? null : (
+					<TextField
+						onBeforeChange={this._textChange}
+						className="trim-search-text"
+					/>
+				)}
+			</span>
+		);
+	}
+}
+
+export default inject("appStore", "trimConnector")(observer(SearchBar));
