946c602f3b1d62811b286a324fea83a1cf03f34d
abani1986 <ranjan.abani@gmail.com>
2019-11-22 11:15:41 +0530

    UnifiedLogViewer Plugin For TailViwer

---
+++ UnifiedLogViewer/UnifiedLogViewer/UnifiedLogViewerPlugins/MyFileFormatPlugin.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using Tailviewer.BusinessLogic.LogFiles;
+using Tailviewer.BusinessLogic.Plugins;
+using Tailviewer.Core.LogFiles;
+
+namespace UnifiedLogViewerPlugins
+{
+    public class MyFileFormatPlugin : IFileFormatPlugin
+    {       
+        public IReadOnlyList<string> SupportedExtensions => new[] { ".log" };
+
+        public ILogFile Open(string fileName, ITaskScheduler taskScheduler)
+        {           
+            //ILogLineTranslator il = new MessageTracerTranslator();
+            //return new Tailviewer.Core.LogFiles.TextLogFile(taskScheduler,fileName,null, il, null);
+
+            return new TextLogFile(taskScheduler, fileName,new MyTimestampParser(),new MyTimestampTranslator());
+        }
+    }
+}
