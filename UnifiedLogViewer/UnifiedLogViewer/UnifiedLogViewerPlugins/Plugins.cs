b0ddb3b39b0b4f87cd16444a69e2909187ebc779
abani1986 <ranjan.abani@gmail.com>
2019-11-22 11:42:03 +0530

    Revert "UnifiedLogViewer Plugin For TailViwer"
    This reverts commit ac59f1c458096017a44aa40b5a09727cbb47705d.

--- UnifiedLogViewer/UnifiedLogViewer/UnifiedLogViewerPlugins/Plugins.cs
+++
@@ -1,7 +0,0 @@
-﻿using Tailviewer.BusinessLogic.Plugins;
-
-[assembly: PluginId("UnifiedLogViewerPlugins", "MicroFocus")]
-[assembly: PluginAuthor("Abani Ranjan")]
-[assembly: PluginWebsite("https://kittyfisto.github.io/Tailviewer/")]
-[assembly: PluginDescription("A plugin to display all logs in a common time stamp format")]
-[assembly: PluginVersion(0, 1)]
