f83b7fd0ca0a9e858290fc3e1f5f42f34a875908
dchurchland <dchurchland@gmail.com>
2020-02-25 16:13:28 +1100

    outlook add - persist selected things in the cache

---
+++ Experimental/word-addin/src/trim-coms/cache-ids.ts
@@ -0,0 +1,13 @@
+export enum CacheIds {
+	UseCheckinStyles = "UseCheckinStyles",
+	CommandDefs = "CommandDefs",
+	LatestQuery = "LatestQuery",
+	DatabaseOptions = "DatabaseOptions",
+	SearchOptions = "SearchOptions",
+	ObjectDef = "ObjectDef",
+	EnumDetails = "EnumDetails",
+	SearchClauses = "SearchClauses",
+	SearchFieldClauses = "SearchFieldClauses",
+	ViewpanePropDefs = "ViewpanePropDefs",
+	Messages = "Messages",
+}
