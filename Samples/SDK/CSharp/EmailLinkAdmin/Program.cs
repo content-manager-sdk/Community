96a37db4ba1e30bdfdd5d40efbd9c5d22e4a6323
dchurchland <dchurchland@gmail.com>
2020-05-20 16:34:12 +1000

    EmailLink admin sample

--- Samples/SDK/CSharp/EmailLinkAdmin/Program.cs
+++ Samples/SDK/CSharp/EmailLinkAdmin/Program.cs
@@ -1,9 +1,6 @@
-﻿using TRIM.ExchangeLink.SyncServer;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿
+using HP.HPTRIM.ExchangeLink.SyncServer;
+
 
 namespace EmailLinkAdmin
 {
@@ -11,26 +8,32 @@ namespace EmailLinkAdmin
 	{
 		static void Main(string[] args)
 		{
-			string workPath = @"C:\HPTRIM\EmailLink";
+			// folder within which the EmailLink preferences JSON is stored
+			string workPath = @"C:\Micro Focus Content Manager\EmailLink";
 
 
 			Preferences preferences = Preferences.Load(workPath);
 
 
-			// Exchange user name and password
-			//var credentialsBasic = new MailDomainCredentials() { Domain = "test", Password = "test pwd", UserName = "test name", MailServer = MailServer.Exchange };
-			//credentialsBasic.EncryptPassword();
-			//preferences.DomainCredentials.Add(credentialsBasic);
+			//Exchange user name and password
+			var credentialsBasic = new MailDomainCredentials() { Domain = "test", Password = "test pwd", UserName = "test name", MailServer = MailServer.Exchange };
+			credentialsBasic.EncryptPassword();
+			preferences.DomainCredentials.Add(credentialsBasic);
 
 
 			// Exchange OAuth
-			var credentialsOAuth = new MailDomainCredentials() { 
-				Domain = "cmofficedev.onmicrosoft.com", 
-				ClientSecret = "ejmg+qZ9-Dk_N-uq1NNXFSGzP5fet2m3", 
-				TenantID= "08363ee4-6592-4325-9d5a-5a25e00d482b", 
-				ProjectID= "09f0ec5c-87e9-4568-8b60-4eb3e20de75e", 
-				MailServer = MailServer.Exchange };
-			credentialsOAuth.EncryptClientSecret();
+			var credentialsOAuth = new MailDomainCredentials()
+			{
+				Domain = "cmofficedev.onmicrosoft.com",
+				ClientSecret = "MY SECRET",
+				TenantID = "MY TENANT ID",
+				ProjectID = "MY PROJECT ID",
+				MailServer = MailServer.Exchange
+			};
+
+			// in a relase subsequent to 9.4 the Client Secret is encryptable and the following should be used to encrypt it.
+			//credentialsOAuth.EncryptClientSecret();
+
 			preferences.DomainCredentials.Add(credentialsOAuth);
 
 
