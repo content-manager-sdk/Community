bb02244ce0304be6ba1e9f0253f88ad7c279fc3a
dchurchland <dchurchland@gmail.com>
2020-08-14 15:05:54 +1000

    support dev version of config

--- Experimental/word-addin/public/config.js
+++ Experimental/word-addin/public/config.js
@@ -2,7 +2,7 @@ window.config = {
 	BASE_URL: "/",
 	SERVICEAPI_PATH: "ServiceAPI",
 	MY_PATH: "/ServiceAPI/office",
-	WEB_CLIENT: "cm",
+	WEB_CLIENT: "ContentManager",
 	DEFAULT_AUTO_OPEN: true,
 	SEARCH_CLAUSES: {
 		Record: [],
