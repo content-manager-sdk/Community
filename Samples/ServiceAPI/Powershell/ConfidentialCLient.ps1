7f26991714d93e7ba966a68ba23e5ad303bbdaef
David Churchland <dchurchland@gmail.com>
2023-02-09 10:13:35 +1100

    new powershell sample

---
+++ Samples/ServiceAPI/Powershell/ConfidentialCLient.ps1
@@ -0,0 +1,32 @@
+﻿#Install MSAL.PS module for all users (requires admin rights)
+#Install-Module MSAL.PS -Scope CurrentUser -Force
+
+#AppId is the Application ID URI from the consumer Azure App (e.g. api://3b30db06-a603-4924-904a-e491a709b6fd)
+$AppId = 'api://xxx-xxx-xxx-xxx-xxx'
+# Tentant Id is the tentant id from Azure AD
+$TenantId = 'xxx-xxx-xxx-xxx-xxx'
+#Client secret comes from your consumer Azure App
+$ClientSecret = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx'
+# the scope id is the Application ID URI from the server Azure App with .default appended
+$scopeId = 'api://MyDev.dev/xxx-xxx-xxx-xxx-xxxx-xxx/.default'
+
+Import-Module MSAL.PS
+$MsalToken = Get-MsalToken -TenantId $TenantId -ClientId $AppId -ClientSecret ($ClientSecret | ConvertTo-SecureString -AsPlainText -Force) -Scopes $scopeId
+
+echo "Access Token found" $MsalToken.AccessToken
+
+#Create Query for Content Manager Service API 
+$url = "https://mydev.dev/serviceapi/Record/9000000001?properties=RecordTitle,RecordNumber&format=json"
+
+## Set up bearer token
+$headers = @{Authorization = "Bearer $($MsalToken.AccessToken)"} 
+
+(Invoke-RestMethod -Uri $Url -ContentType "application/json"  -Method Get -Headers $headers -UseBasicParsing ).Results | Foreach-Object {
+    [pscustomobject] @{
+        Uri = $_.Uri
+        TrimType = $_.TrimType
+        Number = $_.RecordNumber
+        Title = $_.RecordTitle.Value
+       
+    }
+}
---
+++ Samples/ServiceAPI/Powershell/ReadMe.md
@@ -0,0 +1,3 @@
+# Powershell confidential client
+
+The steps to use a confidential client authentication are detailed in the [C# console app sample](https://github.com/content-manager-sdk/Community/tree/master/Samples/ServiceAPI/CSharp/ConsoleServiceAPIClient). This sample demonstrates a powershell script that will connect to the Content Manager ServiceAPI using confidential client authentication.
